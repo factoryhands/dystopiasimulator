7be45dcb71b536690a0ca6a18194ec1329935b06
Jennifer Spriggs <spriggsj@spriggsj-macbookpro.roam.corp.google.com>
2018-09-11 10:59:21 -0700

    adding more scenarios

--- js/main.js
+++ js/main.js
@@ -1,21 +1,29 @@
 var scenarioArray = [
-    'Aliens from another planet have invaded and set up their own global government. Humans have been subjugated and now serve the aliens doing hard labor.',
-    'Dinosaurs are back and terrorize the global population during daylight hours. They are big, they are mean, and they mean business. Theyve made going outside during the daytime absolutely impossible for humans.',
-    'The globe has flooded and land is gone. Those who already owned boats have shaken out to be the leaders of the new global society.',
-    'Pandemic! Half of the population is now zombies. At night they come out and want to eat the other half of the population. Every. Single. Night.',
-    'Evil clones have taken over, and your clone is looking for you and is tasked with your destruction. The clones want to be in charge so they have to get rid of their copy.',
-    'People in the world cant have babies anymore and society is slated to end eventually.',
-
-
+    'there are floods. The land is gone.',
+    'locusts. They eat the world\'s food and foliage.',
+    'aliens have found you. They have remade the world and enslaved humanity.',
+    'dinosaurs have come back. They rule the land, sky, sea and air. Humans are food.',
+    'there is disease and half of the population has become zombies.',
+    'there are clones. You are your clone\'s personal laborer. You exist to do their bidding.',
+    'there are vampires. Humans exist for their blood to be farmed.',
+    'there are no more children. Humanity has stopped being able to reproduce.',
+    'organic food has vanished, and the earth will not produce.',
+    'earth has been long dead, and humanity lives in space in a network of space stations.',
+    'artificial intelligence has become sentient and now algorythmically controls life and humanity.',
+    'people come back from the dead, and resources are scarce.',
+    'there is no more public water resources. Only the rich are able to afford water.',
+    'each consecutive week time rewinds 100 years and humanity is thrust backward into the past.',
+    'it only rains, and there is no sun.',
+    'the sun has died. Water and food are running out.',
 ];
 var randomScenario = scenarioArray[Math.floor(Math.random() * scenarioArray.length)];
 
 var controlArray = [
-    'All communication between humans is illegal and banned. This includes written, verbal, and electronic forms of communication.',
-    'All existing forms of transportation have vanished, and humans are only able to get to where they need to go by walking.',
-    'You are being watched. Survillance chips have been implanted into every human brain and they are used to monitor thought and action. Weekly, at the same time the system goes down for maintenance at a secret time. How can you exploit this?',
-    'Cameras everywhere watch what you do and report back to watchers what you are up to. All acts of rebellion are usually quelled immediately and the detracters snuffed out. You have no idea who is on your side and who is not.',
-    'People cant have families or friends. Society is individualized and it is illegal to band together to form any kind of network. All social activity is monitered and litigated with detractors being immediately snuffed out.'
+    'chips that have been implanted into your brain by the ruling class.',
+    'insects that infect you with a virius that takes away your memory.',
+    'microtransactions for every part of life that you can never get ahead of. To keep up you have to always work with no time off.',
+    'a ruling class that has banned all books and has made reading illegal.',
+    'the state that has banned and made illegal all forms of communication.',
 ];
 var randomControl = controlArray[Math.floor(Math.random() * controlArray.length)];
 
